b88b4cff71a94e8076b6870f0837e8f6017cd1fb
Zaid Khan <zaidbinasif468@gmail.com>
2026-03-29 14:26:02 +0500

    fixs a little things in libs/utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
 
 // Purpose: Shared utility functions used across the entire app
 
